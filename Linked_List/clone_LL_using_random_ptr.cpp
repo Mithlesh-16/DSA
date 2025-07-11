02127578f705cf82e2e539f96b4157a6dfff07ae
MithleshKr-Pro <coding4u2@gmail.com>
2025-07-12 01:38:31 +0530

    Improved some codes

---
+++ Linked_List/clone_LL_using_random_ptr.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* random;
+    Node(int x){
+        this -> data = x;
+        this -> next = nullptr;
+        this -> random = nullptr;
+    }
+};
+
+class Solution{
+    public:
+    Node* clone_LL_using_random_ptr(Node* head){
+        
+    }
+};
+int main() {
+
+
+return 0;
+}
---
+++ Linked_List/merge_sort_LL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+    Node* random;
+    public:
+    Node(int x){
+        this -> data = x;
+        this -> next = nullptr;
+        this -> random = nullptr;
+    }
+};
+
+
+
+int main() {
+
+
+return 0;
+}
