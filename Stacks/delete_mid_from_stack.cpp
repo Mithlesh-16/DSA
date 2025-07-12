58c299d550e889489e21f750641b26d978334a6f
MithleshKr-Pro <coding4u2@gmail.com>
2025-07-12 22:50:50 +0530

    Stack Problem Solved

---
+++ Stacks/delete_mid_from_stack.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void delete_middle_elem(int count, stack<int> &s1, int size){
+    //base case
+    if(count == size/2){
+        s1.pop();
+        return;
+    }
+
+    int num = s1.top();
+    s1.pop();
+    //recursive calls
+    delete_middle_elem(count + 1, s1, size);
+    s1.push(num);
+}
+
+
+int main() {
+
+    stack<int> s1;
+    s1.push(3);
+    s1.push(4);
+    s1.push(2);
+    s1.push(0);
+    s1.push(1);
+
+    int count = 0;
+    delete_middle_elem(count, s1, s1.size());
+
+    while(!s1.empty()){
+        cout<<s1.top()<<endl;
+        s1.pop();
+    }
+
+return 0;
+}
---
+++ Stacks/reverse_str_using_stacks.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main() {
+    string s = "Mithlesh";
+    stack<char> s1;
+    
+    int i = 0;
+    while(s[i]){
+        s1.push(s[i]);
+        i++;
+    }
+
+    s = "";
+    while(--i >= 0){
+        s += s1.top();
+        s1.pop();
+    }
+
+    cout<<s<<endl;
+
+return 0;
+}
